62c0b2c5b52bae6e7c6fc7d87a87a9cfc5e7bebe
yash-ghodasara-91 <yashghodasara91@gmail.com>
2024-11-20 11:47:33 +0530

    Mathematics

---
+++ Methtic/Methetic.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+main () {
+
+    int a;
+    int b;
+    cout <<"input a value:-";
+    cin >> a;
+
+     cout <<"input b value:-";
+    cin >>b;
+
+
+    cout << a+b << "\n";
+    cout << a-b << "\n";
+    cout << a*b << "\n";
+    cout << a/b << "\n";
+    cout << a%b << "\n";
+
+    return 0;
+}
