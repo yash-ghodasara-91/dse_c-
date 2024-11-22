44dd8f08023277d81f8f25095565fb8d9bb8a3f7
yash-ghodasara-91 <yashghodasara91@gmail.com>
2024-11-22 11:21:30 +0530

    Grade

--- if else/pogram-2qustion.cpp
+++ if else/pogram-2qustion.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 
 int main() {
-    // Ask the user to input an integer
+    
+    cout << "--------------------------------------------------------------------\n"; 
     int a;
     cout << "Enter an integer: ";
     cin >> a;
 
-    // Use an if-else statement to determine if the number is odd or even
+   
     if (a % 2 == 0) {
         cout << "The number " << a << " is even." << endl;
     } else {
